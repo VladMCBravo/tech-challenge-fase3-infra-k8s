b62d34e3bd0a882932adc575d7ee829d64bbeb54
Vlad Bravo <vladmcbravo@gmail.com>
2026-09-12 21:07:49 -0300

    fix: substitui modulo eks por recursos nativos para evitar bloqueio 403

--- eks.tf
+++ eks.tf
@@ -1,62 +1,34 @@
 data "aws_caller_identity" "current" {}
 
-module "eks" {
-  source  = "terraform-aws-modules/eks/aws"
-  version = "~> 20.0"
+# 1. Cluster EKS Nativo (Ignora o módulo oficial que causa o erro 403)
+resource "aws_eks_cluster" "oficina_cluster" {
+  name     = "oficina-eks-cluster"
+  role_arn = "arn:aws:iam::${data.aws_caller_identity.current.account_id}:role/LabRole"
+  version  = "1.30"
 
-  cluster_name    = "oficina-eks-cluster"
-  cluster_version = "1.30"
+  vpc_config {
+    subnet_ids             = module.vpc.private_subnets
+    endpoint_public_access = true
+  }
+}
 
-  vpc_id     = module.vpc.vpc_id
-  subnet_ids = module.vpc.private_subnets
+# 2. Grupo de Nós (As máquinas virtuais EC2 que rodam os pods)
+resource "aws_eks_node_group" "oficina_nodes" {
+  cluster_name    = aws_eks_cluster.oficina_cluster.name
+  node_group_name = "oficina-node-group"
+  node_role_arn   = "arn:aws:iam::${data.aws_caller_identity.current.account_id}:role/LabRole"
+  subnet_ids      = module.vpc.private_subnets
 
-  cluster_endpoint_public_access = true
+  instance_types = ["t3.medium"]
 
-  # --- AWS ACADEMY WORKAROUNDS INÍCIO ---
-  
-  # 1. Impede o Terraform de tentar ler a role voclabs (Evita o Erro 403 GetRole)
-  enable_cluster_creator_admin_permissions = false
-
-  # 2. Desativa criação do KMS (AWS Academy bloqueia)
-  create_kms_key            = false
-  cluster_encryption_config = {}
-
-  # 3. Desativa logs do CloudWatch para poupar créditos da AWS Academy
-  cluster_enabled_log_types   = []
-  create_cloudwatch_log_group = false
-
-  # 4. Força o uso da LabRole no Cluster
-  create_iam_role = false
-  iam_role_arn    = "arn:aws:iam::${data.aws_caller_identity.current.account_id}:role/LabRole"
-
-  # 5. Força o uso da LabRole nos Node Groups (Máquinas virtuais do K8s)
-  eks_managed_node_groups = {
-    oficina_nodes = {
-      min_size     = 1
-      max_size     = 2
-      desired_size = 1
-
-      instance_types = ["t3.medium"]
-
-      create_iam_role = false
-      iam_role_arn    = "arn:aws:iam::${data.aws_caller_identity.current.account_id}:role/LabRole"
-    }
+  scaling_config {
+    desired_size = 1
+    max_size     = 2
+    min_size     = 1
   }
 
-  # 6. Garante que você tenha acesso de Admin no cluster usando a role padrão de aluno
-  access_entries = {
-    voclabs_admin = {
-      kubernetes_groups = []
-      principal_arn     = "arn:aws:iam::${data.aws_caller_identity.current.account_id}:role/voclabs"
-      policy_associations = {
-        cluster_admin = {
-          policy_arn = "arn:aws:eks::aws:cluster-access-policy/AmazonEKSClusterAdminPolicy"
-          access_scope = {
-            type = "cluster"
-          }
-        }
-      }
-    }
-  }
-  # --- AWS ACADEMY WORKAROUNDS FIM ---
+  # Diz ao Terraform para só criar as máquinas DEPOIS que o cluster estiver pronto
+  depends_on = [
+    aws_eks_cluster.oficina_cluster
+  ]
 }
